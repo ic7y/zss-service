5deed0815a0d9eee3468fa489e5e1be14ceb3d9a
amigo <wutao25@163.com>
2021-10-14 19:47:44 +0800

    init

---
+++ src/test/java/com/zss/eureka/ZssServiceEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zss.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZssServiceEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
